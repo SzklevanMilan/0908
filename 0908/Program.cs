f7d02cb3d7ec80f47a26084ed2406a5dcf7ba129
SzklevanMilan <szklevanm@gmail.com>
2020-09-08 11:22:11 +0200

    Felhasználó választ

--- 0908/Program.cs
+++ 0908/Program.cs
@@ -18,7 +18,11 @@ namespace _0908
             int gepValasz = vel.Next(0, 3);
             Console.WriteLine("Gép választása:{0}",lehetoseg[gepValasz]);
 
-
+            int jatekosValasz;
+            Console.WriteLine("Kő (0), Papír (1), Olló (2).");
+            Console.Write("Válasz: ");
+            jatekosValasz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Játékos választása: {0}",lehetoseg[jatekosValasz]);
 
             Console.ReadKey();
         }
